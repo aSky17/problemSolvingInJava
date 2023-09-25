61609e468ef0b845355ee5149ad77386d1d97f79
aSky17 <akashmishra.am.1710@gmail.com>
2023-09-25 08:47:24 +0530

    Modified NewSwitchStatement.java

--- NewSwitchStatement.java
+++ NewSwitchStatement.java
@@ -1,4 +1,39 @@
-public class NewSwitchStatement {
+import java.beans.Expression;
 
+public class NewSwitchStatement {
+    public static void main(String[] args) {
+        String day = "Sunday";
+//        switch(day){
+//            case "Sunday","Saturday" -> System.out.println("7am");
+//            case "Friday" -> System.out.println("8am");
+//            default -> System.out.println("6am");
+//
+//        }
+
+//        ----------Switch as an Expression START-------------
+
+        String result = "";
+//        switch(day){
+//            case "Saturday","Sunday" -> result = "7am";
+//            case "Friday" -> result = "8am";
+//            default -> result = "6am";
+//        }
+//        System.out.println(result);
+
+//        result = switch (day){
+//            case "Saturday","Sunday" -> "7am";
+//            case "Friday" -> "8am";
+//            default -> "6am";
+//        };
+
+        result = switch (day){
+            case "Saturday","Sunday" : yield  "7am";
+            case "Friday" : yield "8am";
+            default : yield  "6am";
+        };
+        System.out.println(result);
+//        ----------Switch as an Expression END-------------
+
+    }
 }
 
